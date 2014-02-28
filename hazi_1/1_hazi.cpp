b7c3cfbee56ccc3f341fd8403e43e71810c1d510
Trombitás Péter <trombipeti@gmail.com>
2014-02-28 13:46:51 +0100

    Handling control point adding.

--- hazi_1/1_hazi.cpp
+++ hazi_1/1_hazi.cpp
@@ -1,12 +1,12 @@
 //=============================================================================================
-// Szamitogepes grafika hazi feladat keret. Ervenyes 2014-tol.          
+// Szamitogepes grafika hazi feladat keret. Ervenyes 2014-tol.
 // A //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// sorokon beluli reszben celszeru garazdalkodni, mert a tobbit ugyis toroljuk. 
-// A beadott program csak ebben a fajlban lehet, a fajl 1 byte-os ASCII karaktereket tartalmazhat. 
+// sorokon beluli reszben celszeru garazdalkodni, mert a tobbit ugyis toroljuk.
+// A beadott program csak ebben a fajlban lehet, a fajl 1 byte-os ASCII karaktereket tartalmazhat.
 // Tilos:
 // - mast "beincludolni", illetve mas konyvtarat hasznalni
 // - faljmuveleteket vegezni (printf is fajlmuvelet!)
-// - new operatort hivni az onInitialization függvényt kivéve, a lefoglalt adat korrekt felszabadítása nélkül 
+// - new operatort hivni az onInitialization függvényt kivéve, a lefoglalt adat korrekt felszabadítása nélkül
 // - felesleges programsorokat a beadott programban hagyni
 // - tovabbi kommenteket a beadott programba irni a forrasmegjelolest kommentjeit kiveve
 // ---------------------------------------------------------------------------------------------
@@ -16,12 +16,12 @@
 // ---------------------------------------------------------------------------------------------
 // A feladatmegoldasokban csak olyan gl/glu/glut fuggvenyek hasznalhatok, amelyek
 // 1. Az oran a feladatkiadasig elhangzottak ES (logikai AND muvelet)
-// 2. Az alabbi listaban szerepelnek:  
+// 2. Az alabbi listaban szerepelnek:
 // Rendering pass: glBegin, glVertex[2|3]f, glColor3f, glNormal3f, glTexCoord2f, glEnd, glDrawPixels
-// Transzformaciok: glViewport, glMatrixMode, glLoadIdentity, glMultMatrixf, gluOrtho2D, 
+// Transzformaciok: glViewport, glMatrixMode, glLoadIdentity, glMultMatrixf, gluOrtho2D,
 // glTranslatef, glRotatef, glScalef, gluLookAt, gluPerspective, glPushMatrix, glPopMatrix,
 // Illuminacio: glMaterialfv, glMaterialfv, glMaterialf, glLightfv
-// Texturazas: glGenTextures, glBindTexture, glTexParameteri, glTexImage2D, glTexEnvi, 
+// Texturazas: glGenTextures, glBindTexture, glTexParameteri, glTexImage2D, glTexEnvi,
 // Pipeline vezerles: glShadeModel, glEnable/Disable a kovetkezokre:
 // GL_LIGHTING, GL_NORMALIZE, GL_DEPTH_TEST, GL_CULL_FACE, GL_TEXTURE_2D, GL_BLEND, GL_LIGHT[0..7]
 //
@@ -30,14 +30,14 @@
 // Nev    : Trombitás Péter
 // Neptun : G08HLM
 // ---------------------------------------------------------------------------------------------
-// ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy 
-// mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem. 
-// A forrasmegjeloles kotelme vonatkozik az eloadas foliakat es a targy oktatoi, illetve a 
-// grafhazi doktor tanacsait kiveve barmilyen csatornan (szoban, irasban, Interneten, stb.) erkezo minden egyeb 
-// informaciora (keplet, program, algoritmus, stb.). Kijelentem, hogy a forrasmegjelolessel atvett reszeket is ertem, 
+// ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
+// mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
+// A forrasmegjeloles kotelme vonatkozik az eloadas foliakat es a targy oktatoi, illetve a
+// grafhazi doktor tanacsait kiveve barmilyen csatornan (szoban, irasban, Interneten, stb.) erkezo minden egyeb
+// informaciora (keplet, program, algoritmus, stb.). Kijelentem, hogy a forrasmegjelolessel atvett reszeket is ertem,
 // azok helyessegere matematikai bizonyitast tudok adni. Tisztaban vagyok azzal, hogy az atvett reszek nem szamitanak
-// a sajat kontribucioba, igy a feladat elfogadasarol a tobbi resz mennyisege es minosege alapjan szuletik dontes.  
-// Tudomasul veszem, hogy a forrasmegjeloles kotelmenek megsertese eseten a hazifeladatra adhato pontokat 
+// a sajat kontribucioba, igy a feladat elfogadasarol a tobbi resz mennyisege es minosege alapjan szuletik dontes.
+// Tudomasul veszem, hogy a forrasmegjeloles kotelmenek megsertese eseten a hazifeladatra adhato pontokat
 // negativ elojellel szamoljak el es ezzel parhuzamosan eljaras is indul velem szemben.
 //=============================================================================================
 
@@ -45,18 +45,18 @@
 #include <math.h>
 #include <stdlib.h>
 
-#if defined(__APPLE__)                                                                                                                                                                                                            
-#include <OpenGL/gl.h>                                                                                                                                                                                                            
-#include <OpenGL/glu.h>                                                                                                                                                                                                           
-#include <GLUT/glut.h>                                                                                                                                                                                                            
-#else                                                                                                                                                                                                                             
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)                                                                                                                                                                       
-#include <windows.h>                                                                                                                                                                                                              
-#endif                                                                                                                                                                                                                            
-#include <GL/gl.h>                                                                                                                                                                                                                
-#include <GL/glu.h>                                                                                                                                                                                                               
-#include <GL/glut.h>                                                                                                                                                                                                              
-#endif          
+#if defined(__APPLE__)
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+#include <windows.h>
+#endif
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -68,28 +68,40 @@
 struct Vector {
     float x, y, z;
 
-    Vector( ) { 
+    Vector( )
+    {
         x = y = z = 0;
     }
-    Vector(float x0, float y0, float z0 = 0) { 
-        x = x0; y = y0; z = z0;
+    Vector(float x0, float y0, float z0 = 0)
+    {
+        x = x0;
+        y = y0;
+        z = z0;
     }
-    Vector operator*(float a) { 
-        return Vector(x * a, y * a, z * a); 
+    Vector operator*(float a)
+    {
+        return Vector(x * a, y * a, z * a);
     }
-    Vector operator+(const Vector& v) {
-        return Vector(x + v.x, y + v.y, z + v.z); 
+    Vector operator+(const Vector& v)
+    {
+        return Vector(x + v.x, y + v.y, z + v.z);
     }
-    Vector operator-(const Vector& v) {
-        return Vector(x - v.x, y - v.y, z - v.z); 
+    Vector operator-(const Vector& v)
+    {
+        return Vector(x - v.x, y - v.y, z - v.z);
     }
-    float operator*(const Vector& v) { 	// dot product
-        return (x * v.x + y * v.y + z * v.z); 
+    float operator*(const Vector& v)   	// dot product
+    {
+        return (x * v.x + y * v.y + z * v.z);
     }
-    Vector operator%(const Vector& v) { 	// cross product
+    Vector operator%(const Vector& v)   	// cross product
+    {
         return Vector(y*v.z-z*v.y, z*v.x - x*v.z, x*v.y - y*v.x);
     }
-    float Length() { return sqrt(x * x + y * y + z * z); }
+    float Length()
+    {
+        return sqrt(x * x + y * y + z * z);
+    }
 };
 
 //--------------------------------------------------------
@@ -98,72 +110,77 @@ struct Vector {
 struct Color {
     float r, g, b;
 
-    Color( ) { 
+    Color( )
+    {
         r = g = b = 0;
     }
-    Color(float r0, float g0, float b0) { 
-        r = r0; g = g0; b = b0;
+    Color(float r0, float g0, float b0)
+    {
+        r = r0;
+        g = g0;
+        b = b0;
     }
-    Color operator*(float a) { 
-        return Color(r * a, g * a, b * a); 
+    Color operator*(float a)
+    {
+        return Color(r * a, g * a, b * a);
     }
-    Color operator*(const Color& c) { 
-        return Color(r * c.r, g * c.g, b * c.b); 
+    Color operator*(const Color& c)
+    {
+        return Color(r * c.r, g * c.g, b * c.b);
     }
-    Color operator+(const Color& c) {
-        return Color(r + c.r, g + c.g, b + c.b); 
+    Color operator+(const Color& c)
+    {
+        return Color(r + c.r, g + c.g, b + c.b);
     }
 };
 
 //-----------------------------------------------
-// Kontroll pont. 
+// Kontroll pont
 //-----------------------------------------------
-struct ControlPoint
-{
+struct ControlPoint {
     Vector cp;
     Vector v;
     Vector a;
-    
+
+    long t;
+
     ControlPoint() {};
-    
-    ControlPoint(Vector _cp) : cp(_cp) {}
-    
+
+    ControlPoint(Vector _cp, long _t) : cp(_cp), t(_t) {}
+
     ControlPoint(Vector _cp, Vector _v, Vector _a) :
-    cp(_cp), v(_v), a(_a) {}
+        cp(_cp), v(_v), a(_a) {}
 };
 
-struct RussianSpline
-{
+struct RussianSpline {
     ControlPoint *cps;
     int numCtrlPts;
-    
+
     RussianSpline()
     {
         cps = new ControlPoint[100];
         numCtrlPts = 0;
     }
-    
+
     ~RussianSpline()
     {
         delete[] cps;
     }
-    
-    void addControlPoint(Vector cp)
+
+    void addControlPoint(Vector cp, long _t)
     {
-        if(numCtrlPts < 100)
-        {
-            cps[numCtrlPts++] = cp;
+        if(numCtrlPts < 100) {
+            cps[numCtrlPts++] = ControlPoint(cp,_t);
         }
     }
-    
+
     void drawCtrlPts()
     {
         glColor3f(1.0,1.0,0.0);
         glBegin(GL_POINTS);
-            for(int i = 0;i<numCtrlPts;++i)
-            {
-                glVertex2f(cps[i].cp.x, cps[i].cp.y);
-            }
+        for(int i = 0; i<numCtrlPts; ++i) {
+            glVertex2f(cps[i].cp.x, cps[i].cp.y);
+        }
         glEnd();
         glutSwapBuffers();
     }
@@ -176,14 +193,17 @@ const int screenHeight = 600;
 Color image[screenWidth*screenHeight];	// egy alkalmazás ablaknyi kép
 
 RussianSpline spline;
+
 // Inicializacio, a program futasanak kezdeten, az OpenGL kontextus letrehozasa utan hivodik meg (ld. main() fv.)
-void onInitialization( ) { 
+void onInitialization( )
+{
     glViewport(0, 0, screenWidth, screenHeight);
 
     // Peldakent keszitunk egy kepet az operativ memoriaba
     for(int Y = 0; Y < screenHeight; Y++)
-        for(int X = 0; X < screenWidth; X++)
+        for(int X = 0; X < screenWidth; X++) {
             image[Y*screenWidth + X] = Color((float)X/screenWidth, (float)Y/screenHeight, 0);
+        }
 
 }
 
@@ -198,7 +218,8 @@ float normCoordY(float y)
 }
 
 // Rajzolas, ha az alkalmazas ablak ervenytelenne valik, akkor ez a fuggveny hivodik meg
-void onDisplay( ) {
+void onDisplay( )
+{
     glClearColor(0.1f, 0.2f, 0.3f, 1.0f);		// torlesi szin beallitasa
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // kepernyo torles
 
@@ -222,20 +243,26 @@ void onDisplay( ) {
 }
 
 // Billentyuzet esemenyeket lekezelo fuggveny (lenyomas)
-void onKeyboard(unsigned char key, int x, int y) {
-    if (key == 'd') glutPostRedisplay( ); 		// d beture rajzold ujra a kepet
+void onKeyboard(unsigned char key, int x, int y)
+{
+    if (key == 'd') {
+        glutPostRedisplay( );    // d beture rajzold ujra a kepet
+    }
 
 }
 
 // Billentyuzet esemenyeket lekezelo fuggveny (felengedes)
-void onKeyboardUp(unsigned char key, int x, int y) {
+void onKeyboardUp(unsigned char key, int x, int y)
+{
 
 }
 
-// Eger esemenyeket lekezelo fuggveny
-void onMouse(int button, int state, int x, int y) {
+// Eger esemenyeket lekezelo fuggvenŝy
+void onMouse(int button, int state, int x, int y)
+{
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {  // A GLUT_LEFT_BUTTON / GLUT_RIGHT_BUTTON illetve GLUT_DOWN / GLUT_UP
-        spline.addControlPoint(Vector(normCoordX(x),normCoordY(y)));
+        long tsec = glutGet(GLUT_ELAPSED_TIME);
+        spline.addControlPoint(Vector(normCoordX(x),normCoordY(y)), tsec);
         glutPostRedisplay( ); 						 // Ilyenkor rajzold ujra a kepet
     }
 }
@@ -247,7 +274,8 @@ void onMouseMotion(int x, int y)
 }
 
 // `Idle' esemenykezelo, jelzi, hogy az ido telik, az Idle esemenyek frekvenciajara csak a 0 a garantalt minimalis ertek
-void onIdle( ) {
+void onIdle( )
+{
     long time = glutGet(GLUT_ELAPSED_TIME);		// program inditasa ota eltelt ido
 
 }
@@ -256,9 +284,10 @@ void onIdle( ) {
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // A C++ program belepesi pontja, a main fuggvenyt mar nem szabad bantani
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     glutInit(&argc, argv); 				// GLUT inicializalasa
-    glutInitWindowSize(600, 600);			// Alkalmazas ablak kezdeti merete 600x600 pixel 
+    glutInitWindowSize(600, 600);			// Alkalmazas ablak kezdeti merete 600x600 pixel
     glutInitWindowPosition(100, 100);			// Az elozo alkalmazas ablakhoz kepest hol tunik fel
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);	// 8 bites R,G,B,A + dupla buffer + melyseg buffer
 
@@ -272,7 +301,7 @@ int main(int argc, char **argv) {
     onInitialization();					// Az altalad irt inicializalast lefuttatjuk
 
     glutDisplayFunc(onDisplay);				// Esemenykezelok regisztralasa
-    glutMouseFunc(onMouse); 
+    glutMouseFunc(onMouse);
     glutIdleFunc(onIdle);
     glutKeyboardFunc(onKeyboard);
     glutKeyboardUpFunc(onKeyboardUp);
