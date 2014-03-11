9b4d24dfa144a9a11b1b4e00d2ab081f7a25c1e7
Trombitás Péter <trombipeti@gmail.com>
2014-03-11 10:26:51 +0100

    Egérkattintások kezelése fix. A görberajzolás háttere megvan, de még nem jól rajzol sajnos.

--- hazi_1/1_hazi.cpp
+++ hazi_1/1_hazi.cpp
@@ -6,7 +6,7 @@
 // Tilos:
 // - mast "beincludolni", illetve mas konyvtarat hasznalni
 // - faljmuveleteket vegezni (printf is fajlmuvelet!)
-// - new operatort hivni az onInitialization függvényt kivéve, a lefoglalt adat korrekt felszabadítása nélkül
+// - new operatort hivni az onInitialization függvenyt kiveve, a lefoglalt adat korrekt felszabaditasa nelkül
 // - felesleges programsorokat a beadott programban hagyni
 // - tovabbi kommenteket a beadott programba irni a forrasmegjelolest kommentjeit kiveve
 // ---------------------------------------------------------------------------------------------
@@ -27,7 +27,7 @@
 //
 // NYILATKOZAT
 // ---------------------------------------------------------------------------------------------
-// Nev    : Trombitás Péter
+// Nev    : Trombitas Peter
 // Neptun : G08HLM
 // ---------------------------------------------------------------------------------------------
 // ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
@@ -71,10 +71,11 @@ struct Vector
 {
     float x, y, z;
 
-    Vector( )
+    Vector(float v = 0)
     {
-        x = y = z = 0;
+        x = y = z = v;
     }
+
     Vector(float x0, float y0, float z0 = 0)
     {
         x = x0;
@@ -85,6 +86,12 @@ struct Vector
     {
         return Vector(x * a, y * a, z * a);
     }
+
+    Vector operator/(float a)
+    {
+        return Vector(x / a, y / a, z / a);
+    }
+
     Vector operator+(const Vector& v)
     {
         return Vector(x + v.x, y + v.y, z + v.z);
@@ -93,13 +100,17 @@ struct Vector
     {
         return Vector(x - v.x, y - v.y, z - v.z);
     }
+
     float operator*(const Vector& v)   	// dot product
     {
         return (x * v.x + y * v.y + z * v.z);
     }
+
     Vector operator%(const Vector& v)   	// cross product
     {
-        return Vector(y*v.z-z*v.y, z*v.x - x*v.z, x*v.y - y*v.x);
+        return Vector(y * v.z - z * v.y,
+                      z * v.x - x * v.z,
+                      x * v.y - y * v.x);
     }
     float Length()
     {
@@ -109,6 +120,11 @@ struct Vector
     bool operator==(const Vector& v)
     {
         return (x == v.x && y == v.y && z == v.z);
+    }
+
+    bool operator!=(const Vector& v)
+    {
+        return !(*this == v);
     }
 };
 
@@ -143,67 +159,140 @@ struct Color
     }
 };
 
-//-----------------------------------------------
-// Kontroll pont
-//-----------------------------------------------
-struct ControlPoint
-{
-    Vector cp;
-    Vector v;
-    Vector a;
-
-    long t;
-
-    ControlPoint() {};
-
-    ControlPoint(Vector _cp, long _t) : cp(_cp), t(_t) {}
-
-    ControlPoint(Vector _cp, Vector _v, Vector _a) :
-        cp(_cp), v(_v), a(_a) {}
-};
-
 struct RussianSpline
 {
-    ControlPoint cps[100];
+    Vector cps[100];
     int numCtrlPts;
+    Vector v0;
+    Vector a0;
 
     RussianSpline()
     {
         numCtrlPts = 0;
     }
 
-    void addControlPoint(Vector cp, long _t)
+    void addControlPoint(const Vector& cp)
     {
         if(numCtrlPts < 100)
         {
-            cps[numCtrlPts++] = ControlPoint(cp,_t);
+            cps[numCtrlPts++] = cp;
         }
     }
 
-
-
-    void drawCtrlPts()
+    float t(int i)
     {
+        return cps[i].z - cps[0].z;
+    }
+
+    Vector v(int i )
+    {
+        if(i == 0)
+        {
+            return v0;
+        }
+        else if(i == numCtrlPts - 1)
+        {
+            return Vector(0,0,0);
+        }
+        else
+        {
+            Vector elozo = (cps[i] - cps[i-1]) / (t(i) - t(i-1));
+            Vector kovi  = (cps[i+1] - cps[i]) / (t(i+1) - t(i));
+
+            return (elozo + kovi) * 0.5f;
+        }
+    }
+
+    int index_for_t(float tm)
+    {
+        for(int i = 0;i < numCtrlPts - 1; ++i)
+        {
+            if(t(i+1) > tm)
+            {
+                return i;
+            }
+        }
+        return numCtrlPts - 1;
+    }
+
+
+    Vector r(float _t)
+    {
+        if(_t == 0) return cps[0];
+        int i = index_for_t(_t);
+        if(i == numCtrlPts - 1)
+        {
+            return cps[numCtrlPts - 1];
+        }
+        Vector a[5];
+        a[0] = cps[i];
+        a[1] = v(i);
+        // FIXME Ez itt így egy CM!!!
+        a[2] = (((cps[i+i] - cps[i])*3) / pow(t(i+1) - t(i), 2)) -
+                ( (v(i+1) + v(i)*2) / (t(i+1) - t(i)) );
+        a[3] = (( (cps[i] - cps[i+1])*2 ) / pow(t(i+1) - t(i), 3)) +
+                ( (v(i+1) + v(i)) / pow((t(i+1) - t(i)),2) );
+
+        Vector r;
+        for(int j = 0; j<4; ++j)
+        {
+            r = r + a[j] * pow(_t - (i), j);
+        }
+        return r;
+
+    }
+
+    void draw()
+    {
+        if(numCtrlPts == 0) return;
+        glColor3f(0.0,1.0,0.0);
+        glBegin(GL_TRIANGLE_FAN);
+        for(float angle = 0.0f; angle < 2*M_PI; angle += 0.2f)
+        {
+            float x = (cps[0] - v0).x + sin(angle);
+            float y = (cps[0] - v0).y + cos(angle);
+            glVertex2f(x,y);
+        }
+        glEnd();
+        glBegin(GL_LINE_STRIP);
+            glVertex2f((cps[0] - v0).x, (cps[0] - v0).y);
+            glVertex2f(cps[0].x, cps[0].y);
+        glEnd();
+
+        glColor3f(0.0,0.0,1.0);
+        glBegin(GL_TRIANGLE_FAN);
+        for(float angle = 0.0f; angle < 2*M_PI; angle += 0.2f)
+        {
+            float x = (cps[0] - a0).x + sin(angle);
+            float y = (cps[0] - a0).y + cos(angle);
+            glVertex2f(x,y);
+        }
+        glEnd();
+        glBegin(GL_LINE_STRIP);
+            glVertex2f((cps[0] - a0).x, (cps[0] - a0).y);
+            glVertex2f(cps[0].x, cps[0].y);
+        glEnd();
+
         glColor3f(1.0, 1.0, 1.0);
         for(int i = 0; i<numCtrlPts; ++i)
         {
             glBegin(GL_TRIANGLE_FAN);
             for(float angle = 0.0f; angle < 2*M_PI; angle += 0.2f)
             {
-                float x = cps[i].cp.x + sin(angle);
-                float y = cps[i].cp.y + cos(angle);
+                float x = cps[i].x + sin(angle);
+                float y = cps[i].y + cos(angle);
                 glVertex2f(x,y);
             }
             glEnd();
         }
-        for(int i = 1;i<numCtrlPts; ++i)
+        if(numCtrlPts > 1)
         {
-            glBegin(GL_POINTS);
-            for(long t = 0; t < cps[i].t - cps[i-1].t; ++t)
+            const float res = t(numCtrlPts - 1) - t(0) / 100.0f;
+            glBegin(GL_LINE_STRIP);
+            for(float _time = t(0); _time <= t(numCtrlPts - 1); _time += 0.01)
             {
-                float tabs = (float)t/(cps[i].t - cps[i-1].t);
-                Vector xy = (cps[i-1].cp + (cps[i].cp - cps[i-1].cp)*tabs );
-                glVertex2f(xy.x, xy.y);
+                Vector v = r(_time);
+                glVertex2f(v.x, v.y);
             }
             glEnd();
         }
@@ -213,10 +302,10 @@ struct RussianSpline
 
 typedef enum
 {
-    ADDING_POINTS,      // Kontrollpontok hozzáadása
-    SETTING_UP_VECTORS, // Kezdeti gyorsulás és sebesség beállítása
-    VECTORS_SETUP,      // Készen van minden, lehet nyomni a space-t
-    ANIMATING           // Körbenézegetés
+    ADDING_POINTS,      // Kontrollpontok hozzaadasa
+    SETTING_UP_VECTORS, // Kezdeti gyorsulas es sebesseg beallitasa
+    VECTORS_SETUP,      // Keszen van minden, lehet nyomni a space-t
+    ANIMATING           // Korbenezegetes
 } PROGSTATE;
 
 PROGSTATE prog_state = ADDING_POINTS;
@@ -231,24 +320,30 @@ typedef enum
 } BCLK;
 
 BCLK clickType = NONE;
-long lastClickTime = 0;
+long lastClickTime = -1;
 Vector lastCPPos;
-Vector lastMousePosNorm;
+Vector lastMousePos;
 
 bool startVectorSetup = false;
 bool accelSetup = false;
 
-const int screenWidth = 600;	// alkalmazás ablak felbontása
+const int screenWidth = 600;	// alkalmazas ablak felbontasa
 const int screenHeight = 600;
 
 RussianSpline spline;
 
-float px, py;
-Vector p_eltol;
+bool dont_add = false;
+
+Vector pv;
+Vector pt;
+Vector pa;
+float _mu = -1.28;
 float phi;
 float sx, sy;
 
 double wl, wr, wb, wt;
+
+long eltol_start = 0;
 
 float normCoordX(float x)
 {
@@ -261,13 +356,48 @@ float normCoordY(float y)
 }
 
 
+void updateMouseState()
+{
+    if(lastClickTime < 0 || dont_add) return;
+
+
+    long tmsec = glutGet(GLUT_ELAPSED_TIME);
+    if(tmsec - lastClickTime > 300 && clickType == B1CLK && prog_state == ADDING_POINTS)
+    {
+        spline.addControlPoint(lastCPPos);
+        clickType = NONE;
+    }
+    else if(tmsec - lastClickTime > 300 && clickType == B2CLK && prog_state != VECTORS_SETUP && prog_state != ANIMATING)
+    {
+        prog_state = SETTING_UP_VECTORS;
+        spline.v0 = spline.cps[0] - Vector(lastCPPos.x, lastCPPos.y);
+        startVectorSetup = true;
+        if(accelSetup)
+        {
+            prog_state = VECTORS_SETUP;
+        }
+        clickType = NONE;
+    }
+    else if(clickType == B3CLK && prog_state != VECTORS_SETUP && prog_state != ANIMATING)
+    {
+        prog_state = SETTING_UP_VECTORS;
+        spline.a0 = spline.cps[0] - Vector(lastCPPos.x, lastCPPos.y);
+        accelSetup = true;
+        if(startVectorSetup)
+        {
+            prog_state = VECTORS_SETUP;
+        }
+        clickType = NONE;
+    }
+}
+
+
 // Inicializacio, a program futasanak kezdeten, az OpenGL kontextus letrehozasa utan hivodik meg (ld. main() fv.)
 void onInitialization( )
 {
     glViewport(0, 0, screenWidth, screenHeight);
 
-    px = 0.0f;
-    py = 0.0f;
+    pt = Vector(150,250,0);
     phi = 0.0f;
     sx = 1.0f;
     sy = 1.0f;
@@ -282,14 +412,34 @@ void onInitialization( )
 
 }
 
-
+long prev_draw_time = 0;
 // Rajzolas, ha az alkalmazas ablak ervenytelenne valik, akkor ez a fuggveny hivodik meg
 void onDisplay( )
 {
+    long time = glutGet(GLUT_ELAPSED_TIME);
     glClearColor(0.1f, 0.2f, 0.3f, 1.0f);		// torlesi szin beallitasa
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // kepernyo torles
 
     // ..
+    if(pv != Vector(0,0,0))
+    {
+        float dtime = (time - eltol_start);
+        Vector act_pv = pv + pa*dtime;
+        if(act_pv.x * pv.x > 0 && act_pv.y * pv.y > 0)
+        {
+//                std::cout << "Akutalis sebesseg: " << act_pv.x << "," << act_pv.y << std::endl;
+//                std::cout << "Alap sebesseg: " << pv.x << "," << pv.y << std::endl;
+            pt = pt + act_pv*(dtime/1000.0);
+//                std::cout << "Aktualis eltolas: " << pt.x << "," << pt.y << std::endl;
+//                std::cout << std::endl;
+
+        }
+        else
+        {
+            pv = Vector(0,0,0);
+        }
+
+    }
 
 
     // ...
@@ -300,16 +450,15 @@ void onDisplay( )
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    px += p_eltol.x * 0.3;
-    py += p_eltol.y * 0.3;
-    glTranslatef(px,py,0.0f);
+    glTranslatef(pt.x,pt.y,pt.z); // Az utolso koordinata persze mindig 0 lesz...
     glRotatef(phi,0.0f,0.0f,1.0f);
     glScalef(sx,sy,1.0f);
 
-    spline.drawCtrlPts();
+    spline.draw();
 
 
     glutSwapBuffers();     				// Buffercsere: rajzolas vege
+    prev_draw_time = glutGet(GLUT_ELAPSED_TIME);
 
 }
 
@@ -324,8 +473,29 @@ void onKeyboard(unsigned char key, int x, int y)
     {
         if(prog_state == VECTORS_SETUP || prog_state == ANIMATING)
         {
-            prog_state = ANIMATING;
-            // Animáció indítása...
+//            prog_state = ANIMATING;
+//            pv.x = 0;
+//            pv.y = 0;
+//            pt = Vector(0,0,0);
+//            pa = Vector(0,0);
+            // Animacio inditasa...
+        }
+    }
+    else if (key == 'p')
+    {
+        if(prog_state == VECTORS_SETUP)
+        {
+            pv = (Vector(0,0) - Vector(600,600));
+            pv.x /= 3.0f;
+            pv.y /= 3.0f;
+//            std::cout << pv.x << "," << pv.y << std::endl;
+//            pv.x /= pv.Length();
+//            pv.y /= pv.Length();
+            pv.x *= -1.0f;
+            pa.x = _mu * (pv.x < 0 ? -1 : 1);
+            pa.y = _mu * (pv.y < 0 ? -1 : 1);
+            eltol_start = glutGet(GLUT_ELAPSED_TIME);
+//            std::cout << pv.x << "," << pv.y << std::endl;
         }
     }
 
@@ -342,94 +512,88 @@ void onMouse(int button, int state, int x, int y)
 {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)    // A GLUT_LEFT_BUTTON / GLUT_RIGHT_BUTTON illetve GLUT_DOWN / GLUT_UP
     {
-        long tsec = glutGet(GLUT_ELAPSED_TIME);
+        long tmsec = glutGet(GLUT_ELAPSED_TIME);
 
-        Vector curCPPos( normCoordX(x) * ( (wr - wl) / 2.0 ) + (wr+wl)/2.0 - px,
-                            normCoordY(y) * ( (wt - wb) / 2.0 ) + (wt+wb)/2.0 - py);
-        if(tsec - lastClickTime <= 300 && clickType < B3CLK && curCPPos == lastCPPos)
-        {
-            clickType = (BCLK)((int)clickType + 1);
-        }
-        else
-        {
-            clickType = B1CLK;
-        }
-        lastClickTime = tsec;
-        lastMousePosNorm = Vector(normCoordX(x), normCoordY(y));
-        lastCPPos = curCPPos;
+        Vector curCPPos( normCoordX(x) * ( (wr - wl) / 2.0 ) + (wr+wl)/2.0 - pt.x,
+                            normCoordY(y) * ( (wt - wb) / 2.0 ) + (wt+wb)/2.0 - pt.y,
+                            tmsec / 1000);
+        if      ( clickType == NONE )       clickType = B1CLK;
+        else if ( clickType == B1CLK )      clickType = B2CLK;
+        else if ( clickType == B2CLK )      clickType = B3CLK;
+
+
 //        std::cout << "CLK: " << clickType << ", state: " << prog_state << std::endl;
 
-        switch(clickType)
-        {
-        case B1CLK:
-            if(prog_state == ADDING_POINTS)
-            {
-                spline.addControlPoint(lastCPPos, tsec);
-            }
-            break;
-        case B2CLK:
-            prog_state = SETTING_UP_VECTORS;
-
-            startVectorSetup = true;
-            if(accelSetup)
-            {
-                prog_state = VECTORS_SETUP;
-            }
-            break;
-        case B3CLK:
-            prog_state = SETTING_UP_VECTORS;
-
-            accelSetup = true;
-            if(startVectorSetup)
-            {
-                prog_state = VECTORS_SETUP;
-            }
-            break;
-        default:
-            break;
-        }
+        lastClickTime = tmsec;
+        lastMousePos = Vector(x,y);
+        lastCPPos = curCPPos;
+        updateMouseState();
 
         glutPostRedisplay( ); 						 // Ilyenkor rajzold ujra a kepet
     }
-    else if(state == GLUT_UP && prog_state == VECTORS_SETUP)
+    else if(state == GLUT_UP)
     {
-        long tsec = glutGet(GLUT_ELAPSED_TIME);
-        if(tsec - lastClickTime >= 500 || 1)
+        if( ! (Vector(x,y) == lastMousePos))
         {
-            Vector curMousePos(normCoordX(x), normCoordY(y));
-            if(! (curMousePos == lastMousePosNorm))
-            {
-                clickType = BMOV;
-                px *= ((lastMousePosNorm - curMousePos)).x;
-                py *= ((lastMousePosNorm - curMousePos)).y;
-                std::cout << px << " <---> " << py << std::endl;
-            }
+            clickType = NONE;
         }
+        dont_add = false;
     }
-    glutPostRedisplay();
 }
 
 // Eger mozgast lekezelo fuggveny
 void onMouseMotion(int x, int y)
 {
     long t = glutGet(GLUT_ELAPSED_TIME);
-    std::cout << t - lastClickTime << std::endl;
-    if(t - lastClickTime >= 500 && clickType != BMOV && prog_state == VECTORS_SETUP)
+    if(Vector(x,y) != lastMousePos)
+    {
+        dont_add = true;
+    }
+    if(t - lastClickTime >= 500 && clickType != BMOV /*&& prog_state == VECTORS_SETUP*/)
     {
         clickType = BMOV;
-        Vector curMousePos(normCoordX(x), normCoordY(y));
-        px += ((lastMousePosNorm - curMousePos)).x;
-        py += ((lastMousePosNorm - curMousePos)).y;
-        std::cout << px << " --- " << py << std::endl;
+        Vector curMousePos(x > screenWidth ? screenWidth : x,y > screenHeight ? screenHeight : y);
+        pv = ((curMousePos - lastMousePos)/(3.0));
+        pv.x *= -1.0f;
+        pa.x = _mu * (pv.x < 0 ? -1 : 1);
+        pa.y = _mu * (pv.y < 0 ? -1 : 1);
+        eltol_start = t;
+//        lastMousePos = curMousePos;
+//        std::cout << "Kezdeti eltolas: " << pv.x << " --- " << pv.y << std::endl;
     }
-    glutPostRedisplay();
+//    glutPostRedisplay();
 }
 
 // `Idle' esemenykezelo, jelzi, hogy az ido telik, az Idle esemenyek frekvenciajara csak a 0 a garantalt minimalis ertek
 void onIdle( )
 {
     long time = glutGet(GLUT_ELAPSED_TIME);		// program inditasa ota eltelt ido
-
+    updateMouseState();
+    if(prog_state == VECTORS_SETUP)
+    {
+//        std::cout << "SETUP!!!" << std::endl;
+    }
+//    if(eltol_start > 0 && pv != Vector(0,0,0))
+//    {
+//        long dtime = (time - eltol_start)/1000.0 + 1;
+////        std::cout << "dtime: " << dtime << ", _mu: " << _mu << std::endl;
+//        std::cout << "Eltolas elotte: " << pv.x << " " << pv.y << std::endl;
+//        pv.x *= _mu;
+//        pv.y *= _mu;
+//        if(pv.x < 1/1000.0)
+//        {
+//            pv.x = 0;
+//        }
+//        if(pv.y < 1/1000.0)
+//        {
+//            pv.y = 0;
+//        }
+////        if(! (pv == Vector(0,0,0)))
+////        {
+//            std::cout << "Eltolas utana: " << pv.x << " " << pv.y << std::endl;
+////        }
+//    }
+    glutPostRedisplay();
 }
 
 // ...Idaig modosithatod
